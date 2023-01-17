9fd68d2f34cc6e9eaa89a125282602c3c830b026
Jorge Medina <Jorge@Administrators-MacBook-Pro.local>
2023-01-17 13:49:24 -0500

    pushing files

---
+++ ecommerce/script.js
@@ -0,0 +1,19 @@
+function toggleHamburger () {
+    console.log(`clicked`)
+    var x = document.querySelector("#menuToggle");
+
+    console.log(x);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function cartError () {
+    console.log("click")
+    var x = document.querySelector("")
+    // prompt ("The cart is under renovation. Try again later")
+
+}
